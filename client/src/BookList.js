595a74278e71656513b8078f2fb316f7c0790a0b
Chris Cooley <68765420+chriscooley1@users.noreply.github.com>
2021-02-23 07:14:15 -0700

    used props to populate booklist

---
+++ client/src/BookList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import BookCard from './BookCard';
+
+const BookList = (props) => {
+    return (
+        <div className="list">
+            {
+                props.books.map((book) => {
+                    return <BookCard key={book.id} info={book} />
+            })
+            }
+        </div>
+    );
+}
+
+export default BookList;
