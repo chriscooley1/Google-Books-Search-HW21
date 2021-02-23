294ce22999736e4173d3553dccbef17c2c5df686
Chris Cooley <68765420+chriscooley1@users.noreply.github.com>
2021-02-23 07:19:05 -0700

    updated header to populate search books

---
+++ client/src/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <i className="fas fa-book-open fa-2x"></i>
+            <h1>Search for Books</h1>
+        </header>
+    );
+}
+
+export default Header;
